270ba056722ab007cb666e56f492c73a062843ae
AbdeenM <abdeen.mohamed@outlook.com>
2019-11-19 01:21:24 +0000

    Fixed file indents

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './App.css'
+import logo from './logo.svg'
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+        		</p>
+				<a
+					target='_blank'
+					className='App-link'
+					href='https://reactjs.org'
+					rel='noopener noreferrer'>
+					Learn React
+        		</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
