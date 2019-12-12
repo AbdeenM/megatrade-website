03609d566df606393e6f6499aacf210f079ac9fd
AbdeenM <abdeen.mohamed@outlook.com>
2019-12-12 15:12:43 +0200

    Added Heroku

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,6 +1,12 @@
+/* **************************************************************************
+ * Copyright(C) Mega Trade Website, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Abdeen Mohamed < abdeen.mohamed@outlook.com>, September 2019
+ ************************************************************************** */
 
 export default {
-    SERVER_URL: 'https://localhost:8080',
+    SERVER_URL: 'https://megatrade-server.herokuapp.com',
     FACEBOOK_APP_ID: '442263256673614',
     GOOGLE_CLIENT_ID: '94334450177-alqfl3cs59hmue1ebk5dvtuv973k3qa4.apps.googleusercontent.com'
 }
