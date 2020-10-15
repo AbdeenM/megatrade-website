db1c207148ceeac393463f9fa09cfdc5d18d8f67
AbdeenM <abdeen.mohamed@outlook.com>
2020-10-15 16:14:59 +0200

    Fixed bugs for heroku deployment

---
+++ build/precache-manifest.20cc9e05a8152d31cb8d7954bdd740bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b390196f2eb5a1613512518955c5c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad28b4124531830eb76",
+    "url": "/static/css/2.b76040b9.chunk.css"
+  },
+  {
+    "revision": "0a68fa6cd05c7ac1b6ee",
+    "url": "/static/css/main.625a0633.chunk.css"
+  },
+  {
+    "revision": "2ad28b4124531830eb76",
+    "url": "/static/js/2.88f31954.chunk.js"
+  },
+  {
+    "revision": "554cd45f87671a4de20235dbd77e7f6c",
+    "url": "/static/js/2.88f31954.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a68fa6cd05c7ac1b6ee",
+    "url": "/static/js/main.0dd4888e.chunk.js"
+  },
+  {
+    "revision": "a2bba22f9a3f77ca9733",
+    "url": "/static/js/runtime-main.50218aee.js"
+  }
+]);
